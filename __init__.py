cf4e43ffa24711d6c1583e7a5034503a0bd75cde
Lincoln Stoll <lstoll@lstoll.net>
2009-10-26 18:51:48 +0100

    When checking paths and pushing, see if there is a bananajour repo being served with the same name. If so, make the path bananajour available, which can be used to push to the served repo (equiv. git push bananajour)

--- __init__.py
+++ __init__.py
@@ -20,10 +20,14 @@ This can then be cloned
 '''
 
 import Zeroconf, socket, time, os
-from mercurial import ui
+from mercurial import ui, hg
 from mercurial import extensions
 from mercurial.hgweb import hgweb_mod
 from mercurial.hgweb import hgwebdir_mod
+from mercurial.i18n import _
+
+hg_bananajour_curr_repo = None
+hg_bananajour_reponame = None
 
 def getip():
     # finds external-facing interface without sending any packets (Linux)
@@ -73,7 +77,8 @@ def getzcpaths():
     l = listener()
     Zeroconf.ServiceBrowser(server, "_git._tcp.local.", l)
     # for some reason bananjour is slower here - so we wait for 3 seconds.
-    time.sleep(3)
+    # Todo - why does it need to be this long?
+    time.sleep(5)
     server.close()
     for v in l.found.values():
         n = v.name[:v.name.index('.')]
@@ -82,17 +87,54 @@ def getzcpaths():
         yield "bjr-" + n, u
 
 def config(orig, self, section, key, default=None, untrusted=False):
+    # one of the discovered paths
     if section == "paths" and key.startswith("bjr-"):
         for n, p in getzcpaths():
             if n == key:
                 return p
+    if bjrepo():
+        if section == "paths" and key == "bananajour":
+            # we are currently in a repo, and the path for banajour is 
+            # being requested. so return it.
+            return bjrepo()
     return orig(self, section, key, default, untrusted)
 
 def configitems(orig, self, section, untrusted=False):
     r = orig(self, section, untrusted)
     if section == "paths":
         r += getzcpaths()
+        # check ~/.bananajour/repositories for a repo with the same name as
+        # ours. if it is, list that as a path
+        if bjrepo():
+          r += [('banajour', bjrepo())]
     return r
+    
+def bjrepo():
+    if hg_bananajour_curr_repo:
+        bjrepo = os.path.expanduser('~/.bananajour/repositories/') + hg_bananajour_reponame + '.git'
+        if os.path.isdir(bjrepo):
+          return bjrepo
+        # try again, this time assume we already have the .git
+        bjrepo = os.path.expanduser('~/.bananajour/repositories/') + hg_bananajour_reponame
+        if os.path.isdir(bjrepo):
+          return bjrepo
+
+def bjadd():
+    None
+    # we need to do whatever it is bananjour add does. i suspect
+    # it's just a git init. if so, do the same, and print a message
 
 extensions.wrapfunction(ui.ui, 'config', config)
 extensions.wrapfunction(ui.ui, 'configitems', configitems)
+
+# We need to hook in here to get a repo for later.
+def reposetup(ui, repo):
+  global hg_bananajour_curr_repo, hg_bananajour_reponame
+  if hasattr(repo, 'root'):
+      hg_bananajour_curr_repo = repo
+      hg_bananajour_reponame = os.path.basename(repo.root)
+
+cmdtable = {
+  "bananajour-add":
+        (bjadd, [], _('hg banajour-add')),
+}
